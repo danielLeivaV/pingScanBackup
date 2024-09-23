f4602ace1d6ea5b7172a278a3ba659a318b1bfd5
Daniel Leiva <ignacioleiva79@gmail.com>
2024-09-22 20:41:57 -0400

    Primer commit: Subida inicial del proyecto

---
+++ modules/componentes/navbar.php
@@ -0,0 +1,33 @@
+    <div class="row bg-primary p-2">
+        <div class="col col-1 text-center">
+            <img src="../../public/media/imagenes/icono-hamburguesa.png" alt=""
+                class="icono" />
+        </div>
+
+        <!-- inicio info usuario -->
+        <div class="col-3">
+            <div class="row">
+                <div class="col-3 text-center">
+                    <img src="../../public/media/imagenes/icono-admin.png" alt="Profile" class="icono" />
+                </div>
+                <div class="col-9">
+                    <p class="usuario-icono-info"><b><?php
+                    //mostrar lo que hay en $user()
+                                                        /*echo "<pre>";
+                                                        print_r($user);
+                                                        echo "</pre>";*/
+
+                                                        echo htmlspecialchars($user->nombre); ?></b></p>
+                    <p class="usuario-icono-info"><?php
+                                                    echo htmlspecialchars($user->rol); ?></p>
+                </div>
+            </div>
+        </div>
+        <!-- Fin de info usuario nav-->
+        <div class="col-6"></div>
+        <div class="col-2 btn-cerrar-seccion">
+            <form action="DashboardController.php?action=logout" method="POST">
+                <button class="btn btn-danger" type="submit">Cerrar Sesión</button>
+            </form>
+        </div>
+    </div>
---
+++ modules/dashboard/DashboardView.php
@@ -0,0 +1,81 @@
+<?php require 'DashboardController.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../../public/css/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../../public/css/personalizado.css">
+</head>
+
+<body>
+    <div class="container col-md-12 bg-dark text-light" style="height:100vh;"> <!--div principal-->
+    <?php require '../componentes/navbar.php';?> <!-- llamada al navbar -->
+    <?php if ($user->rol === 'admin'):?>
+    <h2 class="text-center m-2">PANEL PRINCIPAL</h2>
+    <div class="row"><!-- row principal -->
+    <div class="col-4 p-2">
+            <div class="card-opcion">
+            <form method="POST" action="../seguridad_autenticacion/view.php">
+                <button type="submit" class="card-opcion-boton">
+            <img src="../../public/media/imagenes/icono-usuarios.png" alt="Icono para administrar usuarios">
+            <h3 class="card-opcion-h3">Administrar Usuarios</h3>    
+            </button>
+            </form>
+        </div>
+        </div>
+        <div class="col-4 p-2">
+            <div class="card-opcion">
+            <form method="POST" action=""> <!-- definir donde va -->
+                <button type="submit" class="card-opcion-boton">
+            <img src="../../public/media/imagenes/icono-local.png" alt="">
+            <h3 class="card-opcion-h3">Gestionar Locales</h3>    
+            </button>
+            </form>
+        </div>
+        </div>
+        <div class="col-4 p-2">
+            <div class="card-opcion">
+            <form method="POST" action="">
+                <button type="submit" class="card-opcion-boton">
+            <img src="../../public/media/imagenes/icono-red.png" alt="">
+            <h3 class="card-opcion-h3">Gestionar Dispositivos</h3>    
+            </button>
+            </form>
+        </div>
+        </div>
+    </div>
+    
+    <?php endif; ?>
+    
+    
+    
+    
+    
+    
+    <!-- V 1.2.0       
+    <? //php if ($user->rol === 'admin'):?>
+                <h3>Opciones de Administrador</h3>
+                <ul>
+                <li><a href="../seguridad_autenticacion/view.php">Gestión de Usuarios</a></li>
+                </ul>
+            <?php  ?>
+    <?php // if ($user->rol === 'admin' || $user->rol === 'tec' ): ?>
+            <ul>
+            <li><a href="gestion_dispositivos.php">Gestión de Dispositivos</a></li>
+            <?php ?>
+
+            <li><a href="informes.php">Informes Históricos</a></li>
+        </ul>
+
+    -->
+
+
+    
+</div> <!-- cierre de div principal-->
+</body>
+
+</html>
